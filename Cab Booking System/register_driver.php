8334c9e0cd02507cd173b78e4c25ef3081ac181c
Naishal Doshi <naishal036@gmail.com>
2023-08-02 19:56:21 +0530

    Add files via upload

--- Cab Booking System/register_driver.php
+++ Cab Booking System/register_driver.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles_driver.css">
     <title>Register</title>
+    <link rel="stylesheet" href="styles_driver.css">
+    <style>
+        img{
+            height: 150px;
+        }
+    </style>
 </head>
 <body>
-<div class="container">
+<div class="container-fluid">
+<img src="cab-removebg-preview.png" alt="">
         <h1>Register: </h1><hr>
         <form action="" method="post" >
             <div class="form-group">
